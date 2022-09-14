f63b6c19f598d4477645bbe122a64676bcd6c01c
Skreamzero <94937811+Skreamzero@users.noreply.github.com>
2022-09-14 22:10:22 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var main = document.querySelector('main')
+function site(){
+    main.classList.toggle('visible')
+}
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+for (let smoothLink of smoothLinks) {
+    smoothLink.addEventListener('click', function (e) {
+        e.preventDefault();
+        const id = smoothLink.getAttribute('href');
+
+        document.querySelector(id).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+};
